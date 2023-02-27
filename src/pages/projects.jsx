130e9cc89b8bf5343323aa2ffb41468c70bb9279
Daniel <106459193+Chiazor-Daniel@users.noreply.github.com>
2023-02-27 13:37:11 +0100

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom"
 import { data2 } from "../components/dummydata"
 import { AiFillCaretDown } from "react-icons/ai"
